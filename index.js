c4a3d7d367f58697e351b0313b8b6bcffc44c72a
IvambergSilva <ivambergisilva@gmail.com>
2023-08-26 03:38:44 -0300

    :tada: Adiciionando o cors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const jsonServer = require("json-server")
+const jsonServer = require()
 const server = jsonServer.create();
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
@@ -6,5 +6,8 @@ const port = process.env.PORT || 3001
 
 server.use(router)
 server.use(middlewares)
+
+const cors = require("cors")
+server.use(cors())
 
 server.listen(port)
