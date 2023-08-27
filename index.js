e603434688c191191d5634ed3794b321604a043f
IvambergSilva <ivambergisilva@gmail.com>
2023-08-26 22:44:48 -0300

    :tada: Commit inicial

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3001
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Define quem pode acessar (pode ser um domínio específico em vez de '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Define os métodos HTTP permitidos
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Define os headers permitidos
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
     next();
 });
 
 server.use(middlewares)
 server.use(router)
-
-// const cors = require("cors")
-// server.use(cors())
-
 server.listen(port)
